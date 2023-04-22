31574769c9d7c2d5620efadb77ab5821421f9fbb
vadim <gabderahmanov99@gmail.com>
2023-04-22 14:30:47 +0300

    list of credit application

--- src/main/java/com/testtask/bank/controller/BankController.java
+++ src/main/java/com/testtask/bank/controller/BankController.java
@@ -1,19 +1,25 @@
 package com.testtask.bank.controller;
 
+import com.testtask.bank.entity.CreditApp;
+import com.testtask.bank.service.CreditAppService;
 import com.testtask.bank.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BankController {
 
     private CustomerService customerService;
+    private CreditAppService creditAppService;
 
     @Autowired
-    public BankController(CustomerService customerService) {
+    public BankController(CustomerService customerService, CreditAppService creditAppService) {
         this.customerService = customerService;
+        this.creditAppService = creditAppService;
     }
 
     @GetMapping("/main")
@@ -26,6 +32,18 @@ public class BankController {
         return "credit_app_create";
     }
 
+    @PostMapping("/saveCredit")
+    public String insertCredit(@ModelAttribute("credit") CreditApp credit) {
+
+        CreditApp save = creditAppService.save(credit);
+
+        if (save.getApprove() == 0) {
+            return "disapprove_credit";
+        }
+
+        return "redirect:/saveAgreement/"+save.getCustomer().getId();
+    }
+
     @GetMapping("/customer_list")
     public String showCustomerList(Model model) {
         model.addAttribute("customer", customerService.getAll());
@@ -33,7 +51,8 @@ public class BankController {
     }
 
     @GetMapping("/credit_app_list")
-    public String showCreditAppList() {
+    public String showCreditAppList(Model model) {
+        model.addAttribute("credit", creditAppService.getAll());
         return "credit_app_list";
     }
 
