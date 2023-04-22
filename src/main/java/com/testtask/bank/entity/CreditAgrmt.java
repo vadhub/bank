37031145dd7b297c6cf890ffdcfdcea8584d0c9f
vadim <gabderahmanov99@gmail.com>
2023-04-22 13:34:46 +0300

    create entity

---
+++ src/main/java/com/testtask/bank/entity/CreditAgrmt.java
@@ -0,0 +1,65 @@
+package com.testtask.bank.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "credit_agmt")
+public class CreditAgrmt {
+
+    @Id
+    @Column(name = "id_credit_agmt")
+    private Long idAgrmt;
+
+    @Column(name = "date_sigh")
+    private Date dateSigh;
+
+    @Column(name = "sigh")
+    private int sigh;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_credit")
+    private CreditApp creditApp;
+
+    public Date getDateSigh() {
+        return dateSigh;
+    }
+
+    public void setDateSigh(Date dateSigh) {
+        this.dateSigh = dateSigh;
+    }
+
+    public int getSigh() {
+        return sigh;
+    }
+
+    public void setSigh(int sigh) {
+        this.sigh = sigh;
+    }
+
+    public CreditApp getCreditApp() {
+        return creditApp;
+    }
+
+    public void setCreditApp(CreditApp creditApp) {
+        this.creditApp = creditApp;
+    }
+
+    public void setIdAgrmt(Long idAgrmt) {
+        this.idAgrmt = idAgrmt;
+    }
+
+    public Long getIdAgrmt() {
+        return idAgrmt;
+    }
+
+    @Override
+    public String toString() {
+        return "CreditAgrmt{" +
+                "idAgrmt=" + idAgrmt +
+                ", dateSigh=" + dateSigh +
+                ", sigh=" + sigh +
+                ", creditApp=" + creditApp +
+                '}';
+    }
+}
---
+++ src/main/java/com/testtask/bank/entity/Customer.java
@@ -0,0 +1,137 @@
+package com.testtask.bank.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_customer")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "patronymic")
+    private String patronymic;
+
+    @Column(name = "adress")
+    private String adress;
+
+    @Column(name = "seria")
+    private int seria;
+
+    @Column(name = "number")
+    private int number;
+
+    @Column(name = "family_status")
+    private int familyStatus;
+
+    @Column(name = "phone_number")
+    private int phoneNumber;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_work")
+    private Work work;
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", patronymic='" + patronymic + '\'' +
+                ", adress='" + adress + '\'' +
+                ", seria=" + seria +
+                ", number=" + number +
+                ", familyStatus=" + familyStatus +
+                ", phoneNumber=" + phoneNumber +
+                ", work=" + work +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public int getSeria() {
+        return seria;
+    }
+
+    public void setSeria(int seria) {
+        this.seria = seria;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getFamilyStatus() {
+        return familyStatus;
+    }
+
+    public void setFamilyStatus(int familyStatus) {
+        this.familyStatus = familyStatus;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Work getWork() {
+        return work;
+    }
+
+    public void setWork(Work work) {
+        this.work = work;
+    }
+}
