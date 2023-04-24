bfae9045b373859e4a80f237fb304d0a087a9be0
vadim <gabderahmanov99@gmail.com>
2023-04-24 11:31:27 +0300

    random enable

--- src/main/java/com/testtask/bank/service/CreditAppService.java
+++ src/main/java/com/testtask/bank/service/CreditAppService.java
@@ -26,7 +26,7 @@ public class CreditAppService {
     @Transactional
     public CreditApp save(CreditApp credit) {
         Random random = new Random();
-        if (true) {
+        if (random.nextBoolean()) {
             credit.setPeriod((int) (Math.random() * 335) + 30);
             credit.setApprove(1);
         } else {
